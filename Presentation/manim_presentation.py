16dd92d605c387d0b7439133d92a824c3933f84f
artlis321 <58958548+artlis321@users.noreply.github.com>
2023-02-26 17:50:53 +0100

    Updates to test slide

--- Presentation/manim_presentation.py
+++ Presentation/manim_presentation.py
@@ -24,25 +24,93 @@ class TestSlide(ThreeDSlide):
 
     def construct(self):
         axes = ThreeDAxes(x_range=[-4,4], x_length=8)
-        rot = np.pi/2*np.array([1,1,1])
+        rot = np.pi/2*np.array([0,0,0])
 
-        surface = Surface(
-            lambda u,v : axes.c2p(*self.torus_func(u,v,rot)),
-            u_range=[-PI,PI],
-            v_range=[-PI,PI],
-            resolution=16
+        self.set_camera_orientation(phi=PI/4)
+
+        torus = Surface(
+            lambda u,v : self.torus_func(u,v,rot),
+            u_range=[0,TAU],
+            v_range=[0,TAU],
+            resolution=16,
+            checkerboard_colors = [GRAY_D,GRAY_C]
         )
-        self.play( Create(surface) )
+        self.play( Create(torus) )
+        
+        self.wait(2/60)
         self.pause()
 
-        text = Text("THE TORUS WORKS :)")
-        self.add_fixed_in_frame_mobjects(text)
-        self.remove(text)
+        curve_a = ParametricFunction(
+            lambda t : self.torus_func(0,t,rot), color=RED, t_range=[0,TAU]
+        )
+        self.play( Create(curve_a) )
 
-        self.move_camera(phi=PI/2,theta=PI/2,run_time=2)
+        self.wait(2/60)
         self.pause()
 
-        self.play( Write(text,run_time=1) )
+        curve_b = ParametricFunction(
+            lambda t : self.torus_func(t,0,rot), color=BLUE, t_range=[0,TAU]
+        )
+        self.play( Create(curve_b) )
 
+        self.wait(2/60)
         self.pause()
+
+        self.move_camera(frame_center=[6,0,0], zoom=0.6)
+
+        self.wait(2/60)
+        self.pause()
+
+        torus_copy = torus.copy()
+        curve_a1_copy = curve_a.copy()
+        curve_a2_copy = curve_a.copy()
+        curve_b1_copy = curve_b.copy()
+        curve_b2_copy = curve_b.copy()
+
+        flat_func = lambda u,v: np.array([12+PI-v,0+PI-u,0])
+
+        flattened = Surface(
+            lambda u,v : flat_func(u,v),
+            u_range=[0,TAU],
+            v_range=[0,TAU],
+            resolution=16,
+            checkerboard_colors = [GRAY_D,GRAY_C]
+        )
+
+        curve_a1_dest = ParametricFunction(
+            lambda t : flat_func(0,t), color=RED, t_range=[0,TAU]
+        )
+
+        curve_a2_dest = ParametricFunction(
+            lambda t : flat_func(TAU,t), color=RED, t_range=[0,TAU]
+        )
+
+        curve_b1_dest = ParametricFunction(
+            lambda t : flat_func(t,0), color=BLUE, t_range=[0,TAU]
+        )
+
+        curve_b2_dest = ParametricFunction(
+            lambda t : flat_func(t,TAU), color=BLUE, t_range=[0,TAU]
+        )
+
+        self.play(
+            Transform(torus_copy,flattened,run_time=3),
+            Transform(curve_a1_copy,curve_a1_dest,run_time=3),
+            Transform(curve_a2_copy,curve_a2_dest,run_time=3),
+            Transform(curve_b1_copy,curve_b1_dest,run_time=3),
+            Transform(curve_b2_copy,curve_b2_dest,run_time=3)
+        )
+
+        self.wait(2/60)
+        self.pause()
+
+        text = Tex('$\longleftrightarrow$')
+        text.set_x(6)
+
+        self.play( Write(text) )
+
+        self.wait(2/60)
+        self.pause()
+
+        
         
